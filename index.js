b8894c3132eaedb89ec23bf03dd1afe959af8ab6
Alex Dorn <alexmicdorn@gmail.com>
2020-04-27 20:22:14 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const Web3 = require('web3')
-const { toHex, sha3, toChecksumAddress } = require('web3-utils')
+const { sha3, toChecksumAddress, padRight } = require('web3-utils')
 const JOBS = require('./jobs.json')
 const { ORACLE_ADDRESS, RESPONSE_INTERVAL, ALERT_SERVICE, RPC_WSS_URL } = process.env
 
@@ -82,9 +82,10 @@ function main() {
   })
 
   for(let [jobName, jobId] of Object.entries(JOBS)) {
+    console.log(`Creating watcher for ${jobName}. The JOBID is ${jobId}`)
     web3.eth.subscribe('logs', {
       // address: '',
-      topics: ['0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65', toHex(jobId)]
+      topics: ['0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65', padRight('0x' + jobId, 64)]
     }).on('data', (event) => {
       const data = web3.eth.abi.decodeLog([{ type:'bytes32',name:'jobId',indexed:true },{ type:'address',name:'requester' },{ type:'bytes32',name:'requestId', },{ type:'uint256',name:'payment', },{ type:'address',name:'callbackAddr', },{ type:'bytes4',name:'callbackFunctionId', },{ type:'uint256',name:'cancelExpiration', },{ type:'uint256',name:'dataVersion', },{ type:'bytes',name:'data', }],
         event.data,
